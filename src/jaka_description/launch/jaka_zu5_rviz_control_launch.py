d0816d6440b59e0772cbeb87faeebd2c047318b6
Xinjue <hit_zou@126.com>
2026-07-01 16:54:57 +0800

    make description runable

--- src/jaka_description/launch/jaka_zu5_rviz_control_launch.py
+++ src/jaka_description/launch/jaka_zu5_rviz_control_launch.py
@@ -2,15 +2,31 @@ import launch
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration, FindPackageShare
+# from launch.substitutions import LaunchConfiguration, FindPackageShare # Xinjue
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
     # Declare launch arguments if needed (e.g., file paths)
+
+    urdf_file = os.path.join(
+        get_package_share_directory("jaka_description"),
+        "urdf",
+        "jaka_zu5.urdf"
+    )
+    with open(urdf_file, "r") as f:
+        robot_desc = f.read()
+
     return LaunchDescription([
         # Declare the robot_description parameter using a URDF file
         DeclareLaunchArgument(
             'robot_description',
-            default_value=FindPackageShare('jaka_description') + '/urdf/jaka_zu5.urdf',
+            # default_value=FindPackageShare('jaka_description') + '/urdf/jaka_zu5.urdf', # Xinjue
+            default_value=PathJoinSubstitution(
+                [FindPackageShare('jaka_description'), 'urdf', 'jaka_zu5.urdf']
+            ),
             description='Path to the URDF file for the robot'
         ),
 
@@ -19,22 +35,40 @@ def generate_launch_description():
             package='robot_state_publisher',
             executable='robot_state_publisher',
             name='robot_state_publisher',
-            remappings=[('/joint_states', 'robot_jog_command')],
+            # remappings=[('/joint_states', 'robot_jog_command')],
+            parameters=[
+                {"robot_description": robot_desc}
+            ],
+        ),
+
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
         ),
         
-        # Start the jaka_jog_panel node 
-        Node(
-            package='jaka_jog_panel',
-            executable='jakajogpanel',
-            name='jaka_jog_panel',
-        ),
+        # # Start the jaka_jog_panel node # Xinjue
+        # Node(
+        #     package='jaka_jog_panel',
+        #     executable='jakajogpanel',
+        #     name='jaka_jog_panel',
+        # ),
 
         # Start rviz with the provided configuration file
         Node(
             package='rviz2',  # ROS 2 uses rviz2 instead of rviz
             executable='rviz2',
             name='rviz',
-            arguments=['-d', FindPackageShare('jaka_description') + '/config/jaka_zu5_urdf.rviz'],
-            required=True
+            # arguments=['-d', FindPackageShare('jaka_description') + '/config/jaka_zu5_urdf.rviz'], # Xinjue
+            arguments=[
+                '-d',
+                PathJoinSubstitution([
+                    FindPackageShare('jaka_description'),
+                    'config',
+                    'jaka_zu5_urdf.rviz',
+                ]),
+            ],
+            # required=True # Xinjue
+            output='screen',
         ),
     ])
