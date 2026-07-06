e1129be202081d47cc980be5cbc10d3fd0e007b1
Xinjue <hit_zou@126.com>
2026-07-06 15:56:28 +0800

    refactor the controller urdf to meet ros2_control mech; use scaled controller

--- src/jaka_zu5_moveit_config/launch/move_group.launch.py
+++ src/jaka_zu5_moveit_config/launch/move_group.launch.py
@@ -1,7 +1,31 @@
 from moveit_configs_utils import MoveItConfigsBuilder
 from moveit_configs_utils.launches import generate_move_group_launch
-
+from launch import LaunchDescription
+from launch_ros.actions import Node
 
 def generate_launch_description():
     moveit_config = MoveItConfigsBuilder("jaka_zu5", package_name="jaka_zu5_moveit_config").to_moveit_configs()
-    return generate_move_group_launch(moveit_config)
+
+    # MoveIt core
+    move_group_launch = generate_move_group_launch(moveit_config)
+
+    # RViz node
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=["-d", moveit_config.package_path / "config/moveit.rviz"],
+        parameters=[
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.robot_description_kinematics,
+            moveit_config.planning_pipelines,
+            moveit_config.joint_limits,
+        ],
+    )
+
+    return LaunchDescription([
+        move_group_launch,
+        rviz_node
+    ])
